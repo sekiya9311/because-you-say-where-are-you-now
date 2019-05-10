49a7f26f68523bd17a0bbeddb60d0a8fab3758b9
sekiya9311 <sekiya9311@gmail.com>
2019-05-10 18:17:31 +0900

    add SendSettingFragment

---
+++ app/src/main/java/com/sekiya9311/becauseyousaywhereareyounow/viewmodel/SendSettingViewModel.kt
@@ -0,0 +1,7 @@
+package com.sekiya9311.becauseyousaywhereareyounow.viewmodel
+
+import androidx.lifecycle.ViewModel;
+
+class SendSettingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
