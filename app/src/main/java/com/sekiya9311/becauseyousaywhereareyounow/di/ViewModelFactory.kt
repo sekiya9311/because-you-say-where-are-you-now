5614e3a4ec0cfa399b29071315a1a6af48a39134
sekiya9311 <sekiya9311@gmail.com>
2019-06-06 00:03:46 +0900

    Add ViewModelFactory

---
+++ app/src/main/java/com/sekiya9311/becauseyousaywhereareyounow/di/ViewModelFactory.kt
@@ -0,0 +1,30 @@
+package com.sekiya9311.becauseyousaywhereareyounow.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.lang.RuntimeException
+import javax.inject.Inject
+import javax.inject.Provider
+
+class ViewModelFactory @Inject constructor(
+    private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        val creator = creators[modelClass] ?:
+            creators
+                .filter { modelClass.isAssignableFrom(it.key) }
+                .map { it.value }
+                .firstOrNull()
+        creator ?: throw IllegalArgumentException("unknown model class $modelClass")
+
+        try {
+            @Suppress("UNCHECKED_CAST")
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw  RuntimeException(e)
+        }
+    }
+}
