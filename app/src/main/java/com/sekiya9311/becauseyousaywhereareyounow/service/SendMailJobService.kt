9bd18ad69d638dc4ed3cf62fbaf51e5dde8cbfda
sekiya9311 <sekiya9311@gmail.com>
2019-06-20 22:38:30 +0900

    Modify manifest

---
+++ app/src/main/java/com/sekiya9311/becauseyousaywhereareyounow/service/SendMailJobService.kt
@@ -0,0 +1,16 @@
+package com.sekiya9311.becauseyousaywhereareyounow.service
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+
+class SendMailJobService : JobService() {
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
